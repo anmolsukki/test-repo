e1957391ee120b34a0dd3e4878bc103c279b9b81
anmolsukki <anmolsukki94@gmail.com>
2020-05-01 16:54:27 +0530

    remove unused file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
+
 class App extends Component {
   render() {
     return (
